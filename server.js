2a0d60c6a84403b6ffd3d1949678bf6e0ae14a17
DavoSeHe <71927763+DavoSeHe@users.noreply.github.com>
2025-07-03 11:18:18 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-[SERVER_JS_REPLACEMENT]
+const express = require('express');
+const app = express();
+
+// Usa el puerto que Render provee o 3000 en local
+const puerto = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(puerto, () => {
+  console.log(`Servidor escuchando en http://localhost:${puerto}`);
+});
